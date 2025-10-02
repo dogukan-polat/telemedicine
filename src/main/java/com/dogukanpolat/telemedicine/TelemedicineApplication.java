76847b6de12d5727a7a8d6584a0755d3c32e97ec
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-02 20:20:03 +0300

    Initial commit

---
+++ src/main/java/com/dogukanpolat/telemedicine/TelemedicineApplication.java
@@ -0,0 +1,13 @@
+package com.dogukanpolat.telemedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemedicineApplication.class, args);
+    }
+
+}
