0e7ec4d8103701c03587875a7b39d95d004a00d3
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-03 12:56:57 +0300

    Add controller & repository for users

---
+++ src/main/java/com/dogukanpolat/telemedicine/controller/AuthController.java
@@ -0,0 +1,7 @@
+package com.dogukanpolat.telemedicine.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
---
+++ src/main/java/com/dogukanpolat/telemedicine/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dogukanpolat.telemedicine.repository;
+
+import com.dogukanpolat.telemedicine.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByEmail(String email);
+}
