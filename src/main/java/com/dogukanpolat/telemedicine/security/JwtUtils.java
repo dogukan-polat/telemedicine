b79c7d9e218f93ef48c6adbd76ff16d18880de41
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-04 21:08:02 +0300

    Add token validation and get email from token

--- src/main/java/com/dogukanpolat/telemedicine/security/JwtUtils.java
+++ src/main/java/com/dogukanpolat/telemedicine/security/JwtUtils.java
@@ -1,5 +1,7 @@
 package com.dogukanpolat.telemedicine.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,5 +25,29 @@ public class JwtUtils {
                 .expiration(new Date(new Date().getTime() + jwtExpiration * 1000L))
                 .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
                 .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            return !claims.getExpiration().before(new Date());
+
+        } catch (JwtException e) {
+            return  false;
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 }
