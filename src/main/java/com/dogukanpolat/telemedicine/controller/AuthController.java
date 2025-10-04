e842841fc9960b722b7ce2aec771ea5d8471530e
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-04 22:35:39 +0300

    feat: Create login & add validation for it

--- src/main/java/com/dogukanpolat/telemedicine/controller/AuthController.java
+++ src/main/java/com/dogukanpolat/telemedicine/controller/AuthController.java
@@ -1,14 +1,14 @@
 package com.dogukanpolat.telemedicine.controller;
 
-import com.dogukanpolat.telemedicine.dto.user.DoctorRegistrationDto;
-import com.dogukanpolat.telemedicine.dto.user.DoctorResponseDto;
-import com.dogukanpolat.telemedicine.dto.user.PatientRegistrationDto;
-import com.dogukanpolat.telemedicine.dto.user.PatientResponseDto;
+import com.dogukanpolat.telemedicine.dto.user.*;
+import com.dogukanpolat.telemedicine.security.JwtUtils;
 import com.dogukanpolat.telemedicine.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
     private final UserService userService;
+    private final JwtUtils jwtUtils;
 
     @PostMapping("/doctor/register")
     public ResponseEntity<DoctorResponseDto> registerDoctor(@Valid @RequestBody DoctorRegistrationDto doctorRegistrationDto) {
@@ -28,5 +30,18 @@ public class AuthController {
     @PostMapping("/patient/register")
     public ResponseEntity<PatientResponseDto> registerPatient(@Valid @RequestBody PatientRegistrationDto patientRegistrationDto) {
         return new ResponseEntity<>(userService.registerPatient(patientRegistrationDto), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseDto> login(@Valid @RequestBody UserLoginDto userLoginDto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLoginDto.email(),
+                        userLoginDto.password()
+                )
+        );
+
+        String token = jwtUtils.generateToken(userLoginDto.email());
+        return ResponseEntity.ok(new JwtResponseDto(token));
     }
 }
--- src/main/java/com/dogukanpolat/telemedicine/dto/user/UserLoginDto.java
+++ src/main/java/com/dogukanpolat/telemedicine/dto/user/UserLoginDto.java
@@ -1,4 +1,13 @@
 package com.dogukanpolat.telemedicine.dto.user;
 
-public record UserLoginDto(String email, String password) {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserLoginDto(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {}
