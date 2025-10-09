2d848e0cb012c6d9ba44655345ea3a54b0c065c0
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-09 21:58:49 +0300

    feat: Add exception handlers for BadCredentials and HttpMessageNotReadable exceptions

--- src/main/java/com/dogukanpolat/telemedicine/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dogukanpolat/telemedicine/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.dogukanpolat.telemedicine.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,6 +37,26 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("status", "400");
         error.put("error", "Password Mismatch");
+        error.put("message", ex.getMessage());
+        error.put("timestamp", LocalDateTime.now().toString());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredentialsException(BadCredentialsException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("status", "401");
+        error.put("error", "Bad Credentials");
+        error.put("message", ex.getMessage());
+        error.put("timestamp", LocalDateTime.now().toString());
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("status", "400");
+        error.put("error", "Bad Request");
         error.put("message", ex.getMessage());
         error.put("timestamp", LocalDateTime.now().toString());
         return ResponseEntity.badRequest().body(error);
