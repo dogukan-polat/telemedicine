08d804094abd446c738402e16c3d66b2b94e99af
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-18 19:05:31 +0300

    feat: Add user management methods in AdminService
    Introduce methods in `AdminService` to fetch all users, doctors, and patients. Leverage `UserMapper` for mapping and `Role` for filtering.

--- src/main/java/com/dogukanpolat/telemedicine/service/AdminService.java
+++ src/main/java/com/dogukanpolat/telemedicine/service/AdminService.java
@@ -1,15 +1,41 @@
 package com.dogukanpolat.telemedicine.service;
 
+import com.dogukanpolat.telemedicine.dto.admin.UserManagementDto;
+import com.dogukanpolat.telemedicine.mappers.UserMapper;
+import com.dogukanpolat.telemedicine.model.enums.Role;
 import com.dogukanpolat.telemedicine.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public void deleteUser(String email) {
         userRepository.deleteByEmail(email);
+    }
+
+    public List<UserManagementDto> getAllUsers() {
+       return userRepository.findAll().stream()
+               .map(userMapper::toUserManagementDto)
+               .toList();
+    }
+
+    public List<UserManagementDto> getAllDoctors() {
+        return userRepository.findAll().stream()
+                .filter(user -> user.getRole() == Role.DOCTOR)
+                .map(userMapper::toUserManagementDto)
+                .toList();
+    }
+
+    public List<UserManagementDto> getAllPatients() {
+        return userRepository.findAll().stream()
+                .filter(user -> user.getRole() == Role.PATIENT)
+                .map(userMapper::toUserManagementDto)
+                .toList();
     }
 }
