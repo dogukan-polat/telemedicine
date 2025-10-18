f5f1df38c0a33fe775193841745f99851821cece
Doğukan Polat <162631739+dogukan-polat@users.noreply.github.com>
2025-10-18 19:05:49 +0300

    feat: Add endpoints for fetching users, doctors, and patients in AdminController

--- src/main/java/com/dogukanpolat/telemedicine/controller/AdminController.java
+++ src/main/java/com/dogukanpolat/telemedicine/controller/AdminController.java
@@ -1,18 +1,33 @@
 package com.dogukanpolat.telemedicine.controller;
 
+import com.dogukanpolat.telemedicine.dto.admin.UserManagementDto;
 import com.dogukanpolat.telemedicine.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminService adminService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserManagementDto>> getAllUsers() {
+        return ResponseEntity.ok(adminService.getAllUsers());
+    }
+
+    @GetMapping("/patients")
+    public ResponseEntity<List<UserManagementDto>> getAllPatients() {
+        return ResponseEntity.ok(adminService.getAllPatients());
+    }
+
+    @GetMapping("/doctors")
+    public ResponseEntity<List<UserManagementDto>> getAllDoctors() {
+        return ResponseEntity.ok(adminService.getAllDoctors());
+    }
 
     @DeleteMapping("/users/{email}")
     public ResponseEntity<Void> deleteUser(@PathVariable String email) {
